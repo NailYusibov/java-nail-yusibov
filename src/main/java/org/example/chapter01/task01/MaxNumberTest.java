8cac9d0205005db7bb69cc5195e8b3b5e06687dc
NailYusibov <Nani12@bk.ru>
2026-01-12 11:50:20 +0300

    Solution to problem 1 of chapter 1

--- src/main/java/org/example/chapter01/task01/MaxNumberTest.java
+++ src/main/java/org/example/chapter01/task01/MaxNumberTest.java
@@ -1,12 +1,11 @@
 package org.example.chapter01.task01;
 
-import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class MaxNumberTest {
-  @Test
+    @Test
     void testMaxNumber() {
         int result = MaxNumber.max(9, 3, 5);
         assertEquals(9, result);
