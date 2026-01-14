fbc76ff720070b07df493733b20ea81e1385012a
NailYusibov <Nani12@bk.ru>
2026-01-14 13:35:45 +0300

    Fix parameterized tests and improve coverage

--- src/main/java/org/example/chapter01/task01/MaxNumberTest.java
+++ src/main/java/org/example/chapter01/task01/MaxNumberTest.java
@@ -2,12 +2,13 @@ package org.example.chapter01.task01;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-public class MaxNumberTest {
-    @Test
-    void testMaxNumber() {
-        int result = MaxNumber.max(9, 3, 5);
-        assertEquals(9, result);
+class MaxNumberTest {
+    @ParameterizedTest
+    @CsvSource({"1, 9, 5, 9", "9, 3, 5, 9", "5, 3, 9, 9"})
+    void testMaxNumber(int num1, int num2, int num3, int expected) {
+        assertEquals(expected, MaxNumber.max(num1, num2, num3));
     }
 }
