6a2ccdd0a7ff77c32f559230efb855fbb6ff1095
NailYusibov <Nani12@bk.ru>
2026-03-04 11:39:11 +0300

    Corrections based on the rabbit's recommendations

--- src/main/java/org/example/chapter01/task02/BitCounter.java
+++ src/main/java/org/example/chapter01/task02/BitCounter.java
@@ -4,9 +4,11 @@ package org.example.chapter01.task02;
 
 public class BitCounter {
 
+    private static final int LAST_BIT_INDEX = 31;
+
     public int getBitNum(int number, int position) {
-        if (position < 0 || position > 31) {
-            throw new IllegalArgumentException("position must be in range [0, 31]");
+        if (position < 0 || position > LAST_BIT_INDEX) {
+            throw new IllegalArgumentException("position must be in range [0, LAST_BIT_INDEX]");
         }
         return (number >> position) & 1;
     }
